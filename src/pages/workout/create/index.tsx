5d6498aad8de4b2002b4361a93d00ddeb236e005
hyun <zvg7777@gmail.com>
2023-04-30 12:22:51 +0900

    Update CreatePage Text Input Value Alert

--- src/pages/workout/create/index.tsx
+++ src/pages/workout/create/index.tsx
@@ -1,11 +1,15 @@
-function Create() {
-  return (
-    <ul>
-      <li>
-        Create
-      </li>
-    </ul>
-  )
+export default function CreateForm() {
+    const handleSubmit = async (event: any) => {
+      event.preventDefault()
+      alert(event.target.name.value)
+    }
+    
+    return (
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" name="name" />
+  
+        <button type="submit">Submit</button>
+      </form>
+    )
 }
-
-export default Create

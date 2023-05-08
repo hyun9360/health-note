ceb2310fe9440b7e6e3cbfc658da86d2e7353137
hyun <suhyun.lee@sirloin.co.kr>
2023-05-08 09:52:02 +0900

    Update useState 적용 & async 제거

--- src/pages/workout/create/index.tsx
+++ src/pages/workout/create/index.tsx
@@ -1,15 +1,23 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
+
 export default function CreateForm() {
-    const handleSubmit = async (event: any) => {
-      event.preventDefault()
-      alert(event.target.name.value)
-    }
-    
-    return (
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" />
+  const [name, setName] = useState('');
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value)
+  }
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    alert(name)
+  }
   
-        <button type="submit">Submit</button>
-      </form>
-    )
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name</label>
+      <input type="text" id="name" name="name" value={name} onChange={handleChange} />
+
+      <button type="submit">Submit</button>
+    </form>
+  )
 }
